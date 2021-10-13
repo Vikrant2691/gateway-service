da5fefbfb02ca2f757fbab0c0031f4a69e5911de
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2021-10-13 19:00:57 +0530

    added additional properties

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:11
+
+LABEL maintainer="Vikrant Sonawane"
+
+WORKDIR /usr/local/bin/
+
+COPY target/gateway-service-0.0.1-SNAPSHOT.jar gateway-service.jar
+
+EXPOSE 8080
+
+CMD ["java","-jar","discovery-server.jar"]
--- src/test/java/com/vison/project/gateway/GatewayServiceApplicationTests.java
+++ src/test/java/com/vison/project/gateway/GatewayServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.vison.project.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class GatewayServiceApplicationTests {
 
 	@Test
